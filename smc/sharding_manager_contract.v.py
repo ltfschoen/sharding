eb4f02cd7176f1025b7331fbd28acf8c821b6318
jamesray1 <16969914+jamesray1@users.noreply.github.com>
2018-03-28 20:31:06 +1100

    register_collator(), is_stack_empty(), stack_push(), stack_pop etc.

--- smc/sharding_manager_contract.v.py
+++ smc/sharding_manager_contract.v.py
@@ -52,40 +52,15 @@ proposer_lockup_length: int128
 
 collator_pool: public({
 	# array of active collator addresses
-    collator_pool: address[int128]
+    collator_pool_arr: address[int128]
 	# size of the collator pool
     collator_pool_len: int128
-	# Stack of empty collator slot indices caused by the function degister_collator().
+	# Stack of empty collator slot indices caused by the function
+	# degister_collator().
     empty_slots_stack: int128[int128]
 	# The top index of the stack in empty_slots_stack.
     empty_slots_stack_top: int128
 })
-
-@public
-def __init__():
-	# Shards
-	#self.smc_address = 
-	self.network_ID = 0b1000_0001
-	self.shard_count = 100			# shards
-	self.period_length = 5			# block times
-	self.lookahead_length = 4		# periods
-	self.windback_length = 25		# collations
-
-	# Collations
-	self.collation_size	= 1048576	# 2^20 bytes
-	self.chunk_size = 32			# bytes
-	self.collator_subsidy = 0.001 	# vETH
-	self.collator_pool_len = 0		
-    self.collator_pool.empty_slots_stack_top = 0
-
-	# Registries
-	self.collator_deposit = 1000000000000000000000 		# 10^21 wei = 1000 ETH
-	#collator_subsidy = 1000000000000000				# 10^15 wei = 0.001 ETH
-	self.min_proposer_balance = 10000000000000			# 10^17 wei = 0.1 ETH
-	self.collator_lockup_length = 16128					# periods
-	self.proposer_lockup_length = 48					# periods
-    # 10 ** 20 wei = 100 ETH
-    #self.deposit_size = 100000000000000000000
 
 # Collation headers
 collation_headers: public({
@@ -121,6 +96,10 @@ CollationHeaderAdded: __log__({
     #score: num,
 })
 
+
+Registered_collator: __log__([collator_pool.pool_index: int128, __
+	collator_address: address, collator_deposit: wei_value})
+
 # from VMC: TODO: determine the signature of the log `Deposit` and `Withdraw`
 #Deposit: __log__({collator_pool_index: int128, collator_addr: address, deposit: wei_value})
 #Withdraw: __log__({collator_pool_index: int128, collator_addr: address, deposit: wei_value})
@@ -144,6 +123,19 @@ proposer_registry: public ({
 	balances: wei_value[uint256],
 }[proposer_address])
 
+collation_trees_struct: public ({
+	# The collation tree of a shard maps collation hashes to previous collation hashes truncated to 24 bytes packed into a bytes32 with the collation height in the last 8 bytes.
+	collation_trees: bytes32[bytes32][uint256]
+	# This contains the period of the last update for each shard.
+	last_update_periods: int128[uint256]
+})
+
+availability_challenges_struct: public ({
+	# availability_challenges:
+	# availability challenges counter
+	availability_challenges_len: int128
+})
+
 # Number of collators
 #num_collators: public(num)
 
@@ -164,21 +156,124 @@ proposer_registry: public ({
 # Number of receipts
 #num_receipts: num
 
-# Has the validator deposited before?
-# is_validator_deposited: public(bool[address])
+# Has the collator registered before?
+# is_collator_deposited: public(bool[address])
+
+# Has the validator deposited before? This isn't necessary, because of the
+# check assert self.collator_registry[collator_address].pool_index = None
+# in register_collator().
+# has_validator_registered: public(bool[address])
 
 # Log the latest period number of the shard
 #period_head: public(int128[int128])
 
 
+@public
+def __init__():
+	# Shards
+	#self.smc_address = 
+	self.network_ID = 0b1000_0001
+	self.shard_count = 100			# shards
+	self.period_length = 5			# block times
+	self.lookahead_length = 4		# periods
+	self.windback_length = 25		# collations
 
+	# Collations
+	self.collation_size	= 1048576	# 2^20 bytes
+	self.chunk_size = 32			# bytes
+	self.collator_subsidy = 0.001 	# vETH
+	self.collator_pool.collator_pool_len = 0		
+    self.collator_pool.empty_slots_stack_top = 0
 
+	# Registries
+	self.collator_deposit = 1000000000000000000000 		# 10^21 wei = 1000 ETH
+	#collator_subsidy = 1000000000000000				# 10^15 wei = 0.001 ETH
+	self.min_proposer_balance = 10000000000000			# 10^17 wei = 0.1 ETH
+	self.collator_lockup_length = 16128					# periods
+	self.proposer_lockup_length = 48					# periods
+    # 10 ** 20 wei = 100 ETH
+    #self.deposit_size = 100000000000000000000
+
+# Register a collator. Adds an entry to collator_registry, updates the collator pool (collator_pool, collator_pool_len, etc.), locks a deposit of size COLLATOR_DEPOSIT, and returns True on success. Checks:
+
+#    Deposit size: msg.value >= COLLATOR_DEPOSIT
+#    Uniqueness: collator_registry[msg.sender] does not exist
+
+# Checks if empty_slots_stack_top is empty
+@internal
+def is_stack_empty() -> bool:
+    return (self.collator_pool.empty_slots_stack_top == 0)
+
+# Pushes one num to empty_slots_stack
+@internal
+def stack_push(index: int128):
+    self.collator_pool.empty_slots_stack[self.collator_pool.empty_slots_stack_top] = index
+    self.collator_pool.empty_slots_stack_top += 1
+
+# Pops one num out of empty_slots_stack
+@internal
+def stack_pop() -> int128:
+    if self.is_stack_empty():
+        return -1
+    # empty_slots_stack_top_temp = self.empty_slots_stack_top 
+	self.collator_pool.empty_slots_stack_top -= 1
+	self.collator_pool.empty_slots_stack.pop(self.collator_pool.empty_slots_stack_top)
+	#return self.collator_pool.empty_slots_stack[self.collator_pool.empty_slots_stack_top]
+	# https://docs.python.org/3.6/library/array.html?highlight=pop%20array#array.array.pop
+	# TODO: test this, I'm not sure if it's right. You want to return 
+	
+
+@public
+@payable
+def register_collator() -> bool:
+	collator_address = msg.sender
+	assert msg.value >= collator_deposit
+	assert self.collator_registry[collator_address].pool_index = None
+	# Find the empty slot index in the collator pool.
+    if not self.is_stack_empty():
+    	index = self.stack_pop()	
+	else:
+        index = self.collator_pool.collator_pool_len 
+			# collator_pool_arr indices are from 0 to collator_pool_len -1. ;)
+	self.collator_registry[collator_address] = {
+        deregistered: 0,
+        pool_index: index,
+    }
+	self.collator_pool.collator_pool_len +=1
+	self.collator_pool.collator_pool_arr[index] = collator_address
+	
+	log.Registered_collator(index, collator_address, collator_deposit)
+	
+	return True
+
+# Adds a validator to the validator set, with the validator's size being the msg.value
+# (i.e. the amount of ETH deposited) in the function call. Returns the validator index.
+@public
+@payable
+def deposit() -> int128:
+    validator_addr = msg.sender
+    assert not self.is_collator_deposited[validator_addr]
+    assert msg.value == self.deposit_size
+    # find the empty slot index in validators set
+    if not self.is_stack_empty():
+        index = self.stack_pop()
+    else:
+        index = self.num_validators
+    self.validators[index] = {
+        deposit: msg.value,
+        addr: validator_addr,
+    }
+    self.num_validators += 1
+    self.is_validator_deposited[validator_addr] = True
+
+    log.Deposit(index, validator_addr, msg.value)
+
+    return index
 
 # Checks if empty_slots_stack_top is empty
 @internal
 def is_stack_empty() -> bool:
     return (self.empty_slots_stack_top == 0)
-
 
 # Pushes one num to empty_slots_stack
 @internal
